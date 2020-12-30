081f839ab208135092a26edae1f32b7aafca1f23
paulodanieldev <bilkinsdaniel@gmail.com>
2020-12-30 16:37:07 -0300

    refactor(class-wc-pix-gateway)
    corrigindo erros em que os scripts css e js não são chamados na área de pedidos dos clientes

--- includes/js/main.js
+++ includes/js/main.js
@@ -1,34 +1,38 @@
-// jQuery(document).ready( function($) {
-//     if ($("div.wcpix-response-output")){
-//         console.log('div mensagem existe');
-//     }
-// });
+jQuery( "img.wcpix-img-copy-code" ).click(function() {
+    copyCode();
+});
 
-// jQuery( "img.wcpix-img-copy-code" ).click(function($) {
-//     let is_copied = copyCode();
-//     if (is_copied){
-//         if ($("div.wcpix-response-output")){
-//             $("div.wcpix-response-output").show();
-//         }else{
-//             alert('Code copiado!');
-//         }
-//     }else{
-//         alert('Erro ao copiar');
-//     }
-// });
+jQuery( document ).ready(function() {
+    console.log("teste daniel");
+});
 
-// function copyCode() {
-//     try {
-//         var copyText = document.getElementById("copiar");
-//         copyText.type = "text";
-//         copyText.select();
-//         copyText.setSelectionRange(0, 99999);
-//         document.execCommand("copy"); 
-//         copyText.type = "hidden";
-//         return true;
-//     }
-//     catch (e) {
-//         console.log(e);
-//         return false;
-//     }
-// }
+jQuery( "button.wcpix-button-copy-code" ).click(function() {
+    copyCode();
+});
+
+function copyCode() {
+    let result;
+    try {
+        var copyText = document.getElementById("copiar");
+        copyText.type = "text";
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy"); 
+        copyText.type = "hidden";
+        result = true;
+    }
+    catch (e) {
+        console.log(e);
+        result = false;
+    }
+
+    if (result){
+        if (jQuery("div.wcpix-response-output")){
+            jQuery("div.wcpix-response-output").show();
+        }else{
+            alert('Code copiado!');
+        }
+    }else{
+        alert('Erro ao copiar');
+    }
+}
